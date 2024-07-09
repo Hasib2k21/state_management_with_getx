7d91a4d8978f3265051deace2ee37e91574f1fb8
Hasib <hasibislam2k18@gmail.com>
2024-07-09 20:17:45 +0600

    Get Defoultdailog

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -13,20 +13,21 @@ class HomePage extends StatelessWidget {
       body: Center(
         child: ElevatedButton(
           onPressed: () {
-            Get.snackbar(
-              'hi',
-              "Display the message here",
-              colorText: Colors.white,
-              backgroundColor: Colors.blueAccent,
-              icon: const Icon(Icons.add_alert,color: Colors.white,),
-              snackPosition:SnackPosition.BOTTOM,
-              showProgressIndicator: true,
-              borderRadius: 2,
-              margin: const EdgeInsets.all(10),
-              padding: const EdgeInsets.all(5),
-              isDismissible: false,
-              duration: const Duration(seconds: 4),
-              animationDuration: const Duration(milliseconds: 2000)
+            Get.defaultDialog(
+              title: 'delete alert',
+              backgroundColor: Colors.green,
+              titlePadding: EdgeInsets.all(10),
+              titleStyle: TextStyle(color: Colors.black87),
+              middleText: ' are you want to delete ',
+              middleTextStyle: TextStyle(color: Colors.white),
+              radius: 4,
+              textConfirm: 'confirm',
+              textCancel: 'Not now',
+              buttonColor: Colors.indigo,
+              onCancel: ()=>{Get.back(closeOverlays: true)},
+              onConfirm: ()=>{Get.back(closeOverlays: false)},
+              barrierDismissible: true,
+
 
             );
           },
